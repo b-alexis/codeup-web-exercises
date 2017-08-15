53aefbde0ce48e391089c269de212cf717800c71
becca <r.alexis.tolentino@gmail.com>
2017-08-15 09:27:16 -0500

    inline & external JS

---
+++ js/external.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log("Hello from external JavaScript")
+
+alert("welcome to my website.");
+
+var usersfavoriteColor = prompt('What is your favorite color?');
+alert('yay!, '+ usersfavoritecolor +' thats my favorite color too!');
+
