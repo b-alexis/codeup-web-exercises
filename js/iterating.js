5b871f62adff789011a95d3d6b66a2204f95b107
becca <r.alexis.tolentino@gmail.com>
2017-08-18 15:01:01 -0500

    first exercise of arrays

---
+++ js/iterating.js
@@ -0,0 +1,18 @@
+(function(){
+    "use strict";
+var names = ["Rebecca" , "Anthony" , "Benjamin" , "Leonardo"];
+
+    // TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
+
+    // TODO: Create a log statement that will log the number of elements in the names array.
+
+    console.log(names.length);
+    console.log(names[0]);
+    console.log(names[1]);
+    console.log(names[2]);
+    console.log(names[3]);
+
+    // TODO: Create log statements that will print each of the names array elements individually.
+
+
+})();
